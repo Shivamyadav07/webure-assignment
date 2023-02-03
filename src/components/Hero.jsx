f4cf1f38a74236d9cafd9d21e4609d1b0ebf2931
Shivamyadav07 <shivamvnsindia@gmail.com>
2023-02-03 15:03:52 +0530

    developed hero content section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from './Navbar'
+import { HeroContentSection } from './HeroContentSection'
 
 export const Hero = () => {
     return (
         <div className="m-auto border-2 border-black max-w-[1440px] min-h-[780px] bg-[#FFFFFF]">
             <Navbar />
+            <HeroContentSection />
         </div>
     )
 }
---
+++ src/components/HeroContentSection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const HeroContentSection = () => {
+    return (
+        <div className="mx-[135px] h-[481px] w-[646px]">
+            <div className='h-[30px] w-[359px] flex'>
+                <img src='./verify.png' />
+                <p className="font-normal text-[16px] text-[#F17216]">Best Business Platform - World Record 2021</p>
+            </div>
+            <p className="text-[#282C4B] text-[58px] font-bold text-left">Reach Your Business Goal In Record Time</p>
+            <p className="text-[#747582] text-[20px] font-normal text-left my-[25px]">
+                Support small Business and join the nationwide movement
+                to encourage commercial support for the millians of
+                minority owned businesses helping world economy.
+            </p>
+            <div className='flex justify-between my-[70px]'>
+                <button className='text-[18px] font-bold text-[#FFFFFF] h-[68px] w-[210px] border border-[#ffffffcc] rounded-full bg-[#FF698D]'>Get Started</button>
+                <div className='flex items-center'>
+                    <img className='h-[24px] w-[24px]' src='./video-circle.png'/>
+                    <p className='text-[18px] font-medium mx-[4px] '>Best Business Platform - World Record 2021</p>
+                </div>
+            </div>
+        </div>
+    )
+}
