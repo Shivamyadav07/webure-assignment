baee993caf623a29c6506f2ba93db61777374003
Shivamyadav07 <shivamvnsindia@gmail.com>
2023-02-03 16:29:05 +0530

    added hero image section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Navbar'
 import { HeroContentSection } from './HeroContentSection'
+import { HeroImageSection } from './HeroImageSection'
 
 export const Hero = () => {
     return (
-        <div className="m-auto border-2 border-black max-w-[1440px] min-h-[780px] bg-[#FFFFFF]">
+        <div className="m-auto max-w-[1440px] min-h-[780px] bg-[#FFFFFF]">
             <Navbar />
             <HeroContentSection />
+            {/* <HeroImageSection /> */}
         </div>
     )
 }
--- src/components/HeroImageSection.jsx
+++ src/components/HeroImageSection.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export const HeroImageSection = () => {
-  return (
-    <div>HeroImageSection</div>
-  )
+    return (
+        <div className="h-[625px]  w-[596px] absolute z-{0} right-0 ">
+            <img className='absolute left-[25%]' src='./orange.png'/>
+            <img className='absolute bottom-0 left-0 ' src='./blue.png'/>
+            <img className='absolute bottom-[60px] left-[15%]' src='./girl.png'/>
+            <img className='absolute right-0 bottom-[-70px]' src='./review.png'/>
+        </div>
+    )
 }
